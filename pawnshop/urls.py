b3395e84d21b713328e1e498d8534d1df68f242c
Serhii <serhijkrasa64uk5@gmail.com>
2024-12-01 10:18:04 +0200

    edit pawnshop/urls and create basis pawnshop/urls

--- pawnshop/urls.py
+++ pawnshop/urls.py
@@ -1,9 +1,123 @@
 from django.urls import path
 
-from pawnshop.views import index
+from pawnshop.models import Payment, ReferralBonus
+from pawnshop.views import (
+    index,
+    UserListView,
+    UserDetailView,
+    UserCreateView,
+    UserUpdateView,
+    UserDeleteView,
+    ItemListView,
+    ItemDetailView,
+    ItemCreateView,
+    ItemUpdateView,
+    ItemDeleteView,
+    LoanListView,
+    LoanDetailView,
+    LoanCreateView,
+    LoanUpdateView,
+    LoanDeleteView,
+    PaymentListView,
+    PaymentCreateView,
+    PaymentUpdateView,
+    PaymentDeleteView,
+    ReferralBonusListView,
+    ReferralBonusCreateView,
+    ReferralBonusUpdateView,
+    ReferralBonusDeleteView,
+)
 
 app_name = "pawnshop"
 
+
 urlpatterns = [
     path("", index, name="index"),
+    path("users/", UserListView.as_view(), name="user-list"),
+    path(
+        "users/<int:pk>/",
+        UserDetailView.as_view(),
+        name="user-detail"
+    ),
+    path("users/create", UserCreateView.as_view(), name="user-create"),
+    path(
+        "users/<int:pk>/update/",
+        UserUpdateView.as_view(),
+        name="user-update"
+    ),
+    path(
+        "users/<int:pk>/delete",
+        UserDeleteView.as_view(),
+        name="user-delete"
+    ),
+    path("items/", ItemListView.as_view(), name="item-list"),
+    path(
+        "items/<int:pk>/",
+        ItemDetailView.as_view(),
+        name="item-detail"
+    ),
+    path("items/create", ItemCreateView.as_view(), name="item-create"),
+    path(
+        "items/<int:pk>/update/",
+        ItemUpdateView.as_view(),
+        name="item-update"
+    ),
+    path(
+        "items/<int:pk>/delete",
+        ItemDeleteView.as_view(),
+        name="item-delete"
+    ),
+    path("loans/", LoanListView.as_view(), name="loan-list"),
+    path(
+        "loans/<int:pk>/",
+        LoanDetailView.as_view(),
+        name="loan-detail"
+    ),
+    path("loans/create", LoanCreateView.as_view(), name="loan-create"),
+    path(
+        "loans/<int:pk>/update/",
+        LoanUpdateView.as_view(),
+        name="loan-update"
+    ),
+    path(
+        "loans/<int:pk>/delete",
+        LoanDeleteView.as_view(),
+        name="loan-delete"
+    ),
+    path("payments/", PaymentListView.as_view(), name="payment-list"),
+    path(
+        "payments/create",
+        PaymentCreateView.as_view(),
+        name="payment-create"
+    ),
+    path(
+        "payments/<int:pk>/update/",
+        PaymentUpdateView.as_view(),
+        name="payment-update"
+    ),
+    path(
+        "payments/<int:pk>/delete",
+        PaymentDeleteView.as_view(),
+        name="payment-delete"
+    ),
+    path(
+        "referral_bonuses/",
+        ReferralBonusListView.as_view(),
+        name="referral-bonus-list"
+    ),
+    path(
+        "referral_bonuses/create",
+        ReferralBonusCreateView.as_view(),
+        name="referral-bonus-create"
+    ),
+    path(
+        "referral_bonuses/<int:pk>/update/",
+        ReferralBonusUpdateView.as_view(),
+        name="referral-bonus-update"
+    ),
+    path(
+        "referral_bonuses/<int:pk>/delete",
+        ReferralBonusDeleteView.as_view(),
+        name="referral-bonus-delete"
+    ),
 ]
--- pawnshop/views.py
+++ pawnshop/views.py
@@ -1,5 +1,106 @@
-from django.shortcuts import render
+from django.views import generic
+
 
 # Create your views here.
 def index():
+    pass
+
+
+class UserListView(generic.ListView):
+    pass
+
+
+class UserDetailView(generic.DetailView):
+    pass
+
+
+class UserCreateView(generic.CreateView):
+    pass
+
+
+class UserUpdateView(generic.UpdateView):
+    pass
+
+
+class UserDeleteView(generic.DeleteView):
+    pass
+
+
+class ItemListView(generic.ListView):
+    pass
+
+
+class ItemDetailView(generic.DetailView):
+    pass
+
+
+class ItemCreateView(generic.CreateView):
+    pass
+
+
+class ItemUpdateView(generic.UpdateView):
+    pass
+
+
+class ItemDeleteView(generic.DeleteView):
+    pass
+
+
+class LoanListView(generic.ListView):
+    pass
+
+
+class LoanDetailView(generic.DetailView):
+    pass
+
+
+class LoanCreateView(generic.CreateView):
+    pass
+
+
+class LoanUpdateView(generic.UpdateView):
+    pass
+
+
+class LoanDeleteView(generic.DeleteView):
+    pass
+
+
+class PaymentListView(generic.ListView):
+    pass
+
+
+class PaymentDetailView(generic.DetailView):
+    pass
+
+
+class PaymentCreateView(generic.CreateView):
+    pass
+
+
+class PaymentUpdateView(generic.UpdateView):
+    pass
+
+
+class PaymentDeleteView(generic.DeleteView):
+    pass
+
+
+class ReferralBonusListView(generic.ListView):
+    pass
+
+
+class ReferralBonusDetailView(generic.DetailView):
+    pass
+
+
+class ReferralBonusCreateView(generic.CreateView):
+    pass
+
+
+class ReferralBonusUpdateView(generic.UpdateView):
+    pass
+
+
+class ReferralBonusDeleteView(generic.DeleteView):
     pass
