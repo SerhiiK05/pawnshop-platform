a5a4d2d96e7ea6498d02128ce68fc39917990f68
Serhii <serhijkrasa64uk5@gmail.com>
2024-12-02 15:27:09 +0200

    edit forms validation

--- pawnshop/forms.py
+++ pawnshop/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 from pawnshop.models import Item, Loan, Payment, ReferralBonus, User
 
@@ -16,6 +17,11 @@ class ItemForm(forms.ModelForm):
         model = Item
         fields = ["name", "description", "value", "status", "user"]
 
+    def clean_value(self):
+        value = self.cleaned_data["value"]
+        if value < 0:
+            raise ValidationError("Value should be more than 0")
+        return value
 
 class LoanForm(forms.ModelForm):
     class Meta:
@@ -23,12 +29,24 @@ class LoanForm(forms.ModelForm):
         fields = ["total_amount", "interest_rate", "term", "status",
                   "item", "user"]
 
+        def clean_interest_rate(self):
+            interest_rate = self.cleaned_date["interest_rate"]
+            if interest_rate > 100:
+                raise ValidationError("Interest rate should not be more than 100")
+            return interest_rate
+
 
 class PaymentForm(forms.ModelForm):
     class Meta:
         model = Payment
         fields = ["amount", "payment_method", "payment_status",
                   "loan"]
+
+    def clean_amount(self):
+        amount = self.cleaned_data["amount"]
+        if amount < 0:
+            raise ValidationError("Amount should be more than 0")
+        return amount
 
 
 class ReferralBonusForm(forms.ModelForm):
